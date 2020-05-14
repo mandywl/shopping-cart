bd54b2d8bb2b0e6104bb5e62a8263c0a12cafd92
jschrbr <schrbrr@gmail.com>
2020-05-14 22:04:35 +1000

    Simplified api handler

--- controllers/handlers/api.js
+++ controllers/handlers/api.js
@@ -1,51 +1,10 @@
-const { Table1, Table2 } = require("../../models/models");
+const { Users, Products, Orders } = require("../../models/models");
 
 module.exports = {
   getApi: async function(req, res) {
     try {
-      result1 = await Table1.findall();
-      result2 = await Table2.findall();
-      res.json({ ...result1, ...result2 });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err.code });
-    }
-  },
-  createData: async function(req, res) {
-    try {
-      result1 = await Table1.findall();
-      result2 = await Table2.findall();
-      res.json({ ...result1, ...result2 });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err.code });
-    }
-  },
-  readAllData: async function(req, res) {
-    try {
-      result1 = await Table1.findall();
-      result2 = await Table2.findall();
-      res.json({ ...result1, ...result2 });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err.code });
-    }
-  },
-  updateData: async function(req, res) {
-    try {
-      result1 = await Table1.findall();
-      result2 = await Table2.findall();
-      res.json({ ...result1, ...result2 });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err.code });
-    }
-  },
-  deleteData: async function(req, res) {
-    try {
-      result1 = await Table1.findall();
-      result2 = await Table2.findall();
-      res.json({ ...result1, ...result2 });
+      result = await Products.findall();
+      res.json({ ...result });
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: err.code });
