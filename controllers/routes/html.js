db8c3664d10cf393ee5f8b2921d1d878fc707b04
jschrbr <schrbrr@gmail.com>
2020-05-14 20:09:07 +1000

    added login and signup routes for html

--- controllers/routes/html.js
+++ controllers/routes/html.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getIndex } = require("../handlers/html");
+const { getIndex, login, signup } = require("../handlers/html");
 
 router.route("/").get(getIndex);
+router.route("/login").get(login);
+router.route("/signup").get(signup);
 
 module.exports = router;
