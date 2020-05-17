cdfa1d42c4247be4e9f6be5e4f595747bca0c1dd
jschrbr <schrbrr@gmail.com>
2020-05-17 19:54:53 +1000

    fixes #37

--- schema.sql
+++ schema.sql
@@ -4,33 +4,33 @@ DROP DATABASE IF EXISTS eStoreDB;
 CREATE DATABASE eStoreDB;
 
 USE eStoreDB;
-CREATE TABLE users
-(
-    id int NOT NULL AUTO_INCREMENT,
-	first_name varchar (255),
-    last_name varchar (255),
-    email varchar (255) NOT NULL,
-    password varchar (255) NOT NULL,
-    address varchar (255),
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE users
+-- (
+--     id int NOT NULL AUTO_INCREMENT,
+-- 	first_name varchar (255),
+--     last_name varchar (255),
+--     email varchar (255) NOT NULL,
+--     password varchar (255) NOT NULL,
+--     address varchar (255),
+-- 	PRIMARY KEY (id)
+-- );
 
 
-CREATE TABLE products
-(
-    id int NOT NULL AUTO_INCREMENT,
-	product_name varchar (255) NOT NULL,
-    description varchar (255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    img varchar (255) NOT NULL, 
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE products
+-- (
+--     id int NOT NULL AUTO_INCREMENT,
+-- 	product_name varchar (255) NOT NULL,
+--     description varchar (255) NOT NULL,
+--     price DECIMAL(10,2) NOT NULL,
+--     img varchar (255) NOT NULL, 
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE orders
-(
-    id int NOT NULL AUTO_INCREMENT,
-	product_id int NOT NULL,
-    user_id int NOT NULL,
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE orders
+-- (
+--     id int NOT NULL AUTO_INCREMENT,
+-- 	product_id int NOT NULL,
+--     user_id int NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
 
