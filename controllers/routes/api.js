2dc5e41a8ae9d1e431c814d380a66e34571901a4
jschrbr <schrbrr@gmail.com>
2020-05-17 12:55:08 +1000

    Fixed auth routes

--- controllers/routes/api.js
+++ controllers/routes/api.js
@@ -10,8 +10,10 @@ const {
   getProductData,
 } = require("../handlers/api");
 
+const { isAuthenticated } = require("../../config/middleware/authenticate");
+
 router.route("/api/products").get(getApi);
-router.route("/api/orders").get(getOrder);
+router.route("/api/orders").get(isAuthenticated, getOrder);
 router.route("/api/products/:id").get(getProductData);
 router.route("/api/login").post(passport.authenticate("local"), userLogin);
 router.route("/api/signup").post(userSignup);
--- controllers/routes/html.js
+++ controllers/routes/html.js
@@ -7,13 +7,15 @@ const {
   signup,
   getProductPage,
 } = require("../handlers/html");
+
 const {
+  isAuthenticated,
   checkAuthenticated,
   isNotAuthenticated,
 } = require("../../config/middleware/authenticate");
 
 router.route("/").get(checkAuthenticated, getIndex);
-router.route("/orders").get(checkAuthenticated, getOrder);
+router.route("/orders").get(isAuthenticated, getOrder);
 router.route("/login").get(isNotAuthenticated, login);
 router.route("/signup").get(isNotAuthenticated, signup);
 router.route("/product/:id").get(checkAuthenticated, getProductPage);
