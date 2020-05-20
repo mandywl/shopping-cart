e72f1782cd13c6322f664719674b80c56c3e2306
jschrbr <schrbrr@gmail.com>
2020-05-20 19:32:47 +1000

    added footer with some links

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -56,7 +56,6 @@ const handleProduct = async (id) => {
 $(document).ready(function() {
   const elem = $(".carousel");
   var instance = M.Carousel.getInstance(elem);
-
   setInterval(() => {
     instance.next();
   }, 5000);
