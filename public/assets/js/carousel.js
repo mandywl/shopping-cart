e4dc6e8e9fc694a7552b50b29e0c921f97924b64
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-05-21 19:36:43 +1000

    updated carousel displaying speeding to 5 seconds

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -16,7 +16,7 @@ function showSlides() {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " dot-active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 showSlides();
